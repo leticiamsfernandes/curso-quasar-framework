af72c17d52f60fdfcdf158941711467fe97be185
leticiamsfernandes <lemayara16@gmail.com>
2021-10-26 10:09:34 -0300

    instalação

---
+++ exemplo/src/pages/Exemplo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Exemplo Page
+  </div>
+</template>
---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-tabs v-model="tab" class="text-teal">
+      <q-tab name="mails" icon="mail" label="Mails" />
+      <q-tab name="alarms" icon="alarm" label="Alarms" />
+      <q-tab name="movies" icon="movie" label="Movies" />
+    </q-tabs>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "HelloWorld",
+  setup() {
+    return {
+      tab: ref("mails"),
+    };
+  },
+};
+</script>
